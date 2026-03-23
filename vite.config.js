e7e42ec03f8f257919ec6223a6656706ef425742
Bjørn Højland <bh@schilling.dk>
2026-03-23 09:53:02 +0100

    Fix vite config v2

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,7 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-export default defineConfig({
-  plugins: [react()],
-  base: '/kalorieapp/',
-})
-```
-
-Gem, og kør:
-```
-git add vite.config.js
-git commit -m "Fix vite config"
-git push
+import { defineConfig } from 'vite'  
+import react from '@vitejs/plugin-react'  
+  
+export default defineConfig({  
+  plugins: [react()],  
+  base: '/kalorieapp/',  
+}) 
